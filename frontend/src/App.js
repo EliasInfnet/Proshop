8cd5af20e14444a53a60266464ec54ee1b053184
EliasInfnet <elias.carvalho@al.infnet.edu.br>
2022-07-24 03:25:09 -0300

    frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,24 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Container } from 'react-bootstrap'
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <HomeScreen/>
+          <Routes>
+            <Route path='/' element={<HomeScreen/>} />
+            <Route path='/product/:id' element={<ProductScreen/>}/>
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 }
 
