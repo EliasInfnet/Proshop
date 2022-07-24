2950011f53236ca20ea800e01c7f0a350ceda36c
EliasInfnet <elias.carvalho@al.infnet.edu.br>
2022-07-23 23:06:27 -0300

    commit 01

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,18 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from 'react-bootstrap'
+import HomeScreen from "./screens/HomeScreen";
+
 function App() {
   return (
     <>
-      <h1>Welcome to ProShop</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <HomeScreen/>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
