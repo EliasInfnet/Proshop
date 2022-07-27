eac6f904aa919095ba040e7a17ef4c4354bdcca5
EliasInfnet <elias.carvalho@al.infnet.edu.br>
2022-07-27 00:51:33 -0300

    middleware errorHandler

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,17 @@ import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
 import connectDB from './config/db.js'
+import { notFound, errorHanlder } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
 
 dotenv.config()
-
 connectDB()
-
 const app = express()
+
 app.use('/api/products', productRoutes)
+
+app.use(notFound)
+app.use(errorHanlder)
 
 app.get('/', (req, res) => {
   res.send('API is running')
