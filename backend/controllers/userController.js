02d10eba27c17c683c5f025809ed03a74e38ef95
EliasInfnet <elias.carvalho@al.infnet.edu.br>
2022-07-30 19:32:13 -0300

    jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import User from '../models/userModel.js'
+import generateToken from '../utils/generateToken.js'
 import asyncHandler from 'express-async-handler'
 
 //@desc Auth user & get token
@@ -6,6 +7,7 @@ import asyncHandler from 'express-async-handler'
 //@access Public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body
+  
   const user = await User.findOne({ email })
 
   if (user && (await user.matchPassword(password))) {
@@ -14,7 +16,7 @@ const authUser = asyncHandler(async (req, res) => {
       name: user.name,
       email: user.email,
       isAdmin: user.isAdmin,
-      token: null
+      token: generateToken(user._id)
     })
   } else {
     res.status(401)
