5bd2858d599c28642e2e20a49ee785b66492f350
EliasInfnet <elias.carvalho@al.infnet.edu.br>
2022-07-30 19:51:36 -0300

    middleware auth

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,11 +3,11 @@ import generateToken from '../utils/generateToken.js'
 import asyncHandler from 'express-async-handler'
 
 //@desc Auth user & get token
-//@route POST /api/user/login
+//@route POST /api/users/login
 //@access Public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body
-  
+
   const user = await User.findOne({ email })
 
   if (user && (await user.matchPassword(password))) {
@@ -22,7 +22,25 @@ const authUser = asyncHandler(async (req, res) => {
     res.status(401)
     throw new Error('Invalid email or password')
   }
-
 })
 
-export { authUser }
+//@desc Get user Profile
+//@route GET /api/users/profile
+//@access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin
+    })
+  } else {
+    res.status(401)
+    throw new Error('User not found')
+  }
+})
+
+export { authUser, getUserProfile }
