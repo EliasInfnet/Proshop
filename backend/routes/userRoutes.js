de44e25f8d0809cf542eda85ace216b72327c4bf
EliasInfnet <elias.carvalho@al.infnet.edu.br>
2022-08-08 23:09:37 -0300

    auth middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,12 @@
 import express from 'express'
-import { authUser, getUserProfile, registerUser, updateUserProfile } from '../controllers/userController.js'
-import { protect } from '../middleware/authMiddleware.js'
+import { authUser, getUserProfile, registerUser, updateUserProfile, getUsers } from '../controllers/userController.js'
+import { protect, admin } from '../middleware/authMiddleware.js'
 
 const router = express.Router()
 
 router.post('/login', authUser)
 router.route('/profile').get(protect, getUserProfile)
-router.route('/').post(registerUser)
-router.route('/profile').get(protect, getUserProfile).put(protect,updateUserProfile)
+router.route('/').post(registerUser).get(protect, admin, getUsers)
+router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile)
 
 export default router
