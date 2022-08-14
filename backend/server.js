8933157c30706d3b50429b3d323f27e130f64436
EliasInfnet <elias.carvalho@al.infnet.edu.br>
2022-08-14 05:35:21 -0300

    upload image back

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import colors from 'colors'
+import path from 'path'
 import connectDB from './config/db.js'
 import { notFound, errorHanlder } from './middleware/errorMiddleware.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 
 dotenv.config()
 connectDB()
@@ -16,12 +17,16 @@ app.use(express.json())
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
+
 app.get('/api/config/paypal', (req, res) =>
   res.send(process.env.PAYPAL_CLIENT_ID)
 )
+
+const __dirname = path.resolve()
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 app.use(notFound)
 app.use(errorHanlder)
-
 
 app.get('/', (req, res) => {
   res.send('API is running')
