a81e01eb10fcb47abdf0f2e1566786c43d320fac
EliasInfnet <elias.carvalho@al.infnet.edu.br>
2022-08-15 02:27:40 -0300

    review back

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { getProducts, getProductById, deleteProduct, updateProduct, createProduct } from '../controllers/productController.js'
+import { getProducts, getProductById, deleteProduct, updateProduct, createProduct, createProductReview } from '../controllers/productController.js'
 import { admin, protect } from '../middleware/authMiddleware.js'
 const router = express.Router()
 
@@ -9,5 +9,7 @@ router.route('/:id')
   .get(getProductById)
   .delete(protect, admin, deleteProduct)
   .put(protect, admin, updateProduct)
+
+router.route('/:id/reviews').post(protect, createProductReview)
 
 export default router
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import morgan from 'morgan'
 import path from 'path'
 import connectDB from './config/db.js'
 import { notFound, errorHanlder } from './middleware/errorMiddleware.js'
@@ -12,6 +13,10 @@ import uploadRoutes from './routes/uploadRoutes.js'
 dotenv.config()
 connectDB()
 const app = express()
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
 
 app.use(express.json())
 
